14d77894749572d9b59de596c62b8858ffe1decc
Ruben MM <rubenmedinamartinez2016@gmail.com>
2024-12-11 12:12:17 +0100

    push first part 2

---
+++ ft_calloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubmedin <rubmedin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 11:25:08 by rubmedin          #+#    #+#             */
+/*   Updated: 2024/12/11 11:59:18 by rubmedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	char	*ptr_str;
+	size_t	i;
+
+	ptr = malloc(nmemb * size);	
+	if (!ptr)
+		return (NULL);
+	ptr_str = (char *)ptr;
+
+	i = 0;
+	while (i < (nmemb * size))
+	{
+		ptr_str[i] = '\0';
+		i++;
+	}
+	return (ptr);
+}
---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubmedin <rubmedin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 12:08:38 by rubmedin          #+#    #+#             */
+/*   Updated: 2024/12/11 12:11:16 by rubmedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+char	*ft_strdup(const char *s)
+{
+	char *str;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s);
+	str = malloc(sizeof(char) * len);
+	i = 0;
+	while(s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}	
+	str[i] = '\0';
+	return (str);
+}
