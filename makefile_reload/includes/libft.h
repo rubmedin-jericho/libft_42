9baedec1dbea6f0ed117ed06521b9656d4b5dbc3
Ruben MM <rubenmedinamartinez2016@gmail.com>
2024-12-27 10:57:54 +0100

    push

---
+++ makefile_reload/includes/libft.h
@@ -0,0 +1,12 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int	ft_strcmp(const char *s1, const char *s2);
+size_t	strlen(const char *s);
+void	ft_swap(int *nb1, int *nb2);
+
+#endif
